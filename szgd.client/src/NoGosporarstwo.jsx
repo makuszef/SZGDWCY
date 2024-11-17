eaeec422199aaa94051da5e6f342921021d8f557
Hubert <hu.makowski@gmail.com>
2024-11-17 16:51:30 +0100

    Przekierowywanie niezalogowanych na /login

--- szgd.client/src/NoGosporarstwo.jsx
+++ szgd.client/src/NoGosporarstwo.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Alert, AlertTitle, Box} from '@mui/material';
 import {useAuth} from "@/AuthContext.jsx";
-
+import { useNavigate } from 'react-router-dom';
 const NoGospodarstwoAlert = () => {
     const { user } = useAuth(); // Access user data from the auth context
     console.log(user);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user?.userdata) {
+            // Redirect the user if the condition is met
+            navigate('/login');
+        }
+    }, [user]);
     return (
         <Box>
             {!user?.userdata ? 
